eed75a13d37efc192bf06797737cc8dec938fd16
Dorbmon <1263592223@qq.com>
2018-04-15 15:27:02 +0800

    ....

--- go_pkg/pkg_load/pkg_load_!windows.go
+++ go_pkg/pkg_load/pkg_load_!windows.go
@@ -1,6 +1,16 @@
 // +build !windows
 
 package pkg_load
+type LoadedFileFunc struct{
+	//func_map map[string]*plugin.Symbol
+	FilePointer *plugin.Plugin
+	has bool
+}
+type JSLoader struct{
+	LoadedFile map[string]*LoadedFileFunc
+	js *otto.Otto
+}
 func (this *JSLoader)SwapJS(engine *otto.Otto){
 	//Do nothing......We will suport this soon...
+	this.js = engine
 }
