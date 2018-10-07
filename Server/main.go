aee27dc9cf24b7f741d6de5abb49ffce78358136
Dorbmon <1263592223@qq.com>
2018-10-07 15:23:37 +0800

    Add RJS Online Running Server.

--- Server/main.go
+++ Server/main.go
@@ -1,17 +1,15 @@
 package main
 
 import "net/http"
-import "encoding/base64"
 import (
-	"github.com/dorbmon/R.JS/engine"
-	"github.com/dorbmon/otto"
+	"github.com/Dorbmon/R.JS/engine"
+	"github.com/Dorbmon/otto"
 	"math/rand"
 	"time"
+	//"encoding/base64"
+	"fmt"
+	"encoding/base64"
 )
-const (
-	base64Table = "123QRSTUabcdVWXYZHijKLAWDCABDstEFGuvwxyzGHIJklmnopqr234560178912"
-)
-var coder = base64.NewEncoding(base64Table)
 func main(){
 	//创建服务器
 	http.HandleFunc("/",Run)
@@ -19,48 +17,73 @@ func main(){
 }
 func Run(w http.ResponseWriter, r *http.Request){
 	//获得程序
-	code,err := base64Decode([]byte(r.Form.Get("code")))
+	w.Header().Set("Access-Control-Allow-Origin", "*")//允许访问所有域
+	w.Header().Add("Access-Control-Allow-Headers","Content-Type")//header的类型
+	w.Header().Set("content-type","application/json")//返回数据格式是json
+	//code,err := base64Decode([]byte(r.Form.Get("code")))
+	err := r.ParseForm()
+	if err != nil{
+		fmt.Println(err)
+		return
+	}
+	decodeBytes, err := base64.StdEncoding.DecodeString(r.Form.Get("code"))
 	if err != nil{
 		w.Write([]byte(err.Error()))
 		return
 	}
+	code := string(decodeBytes)
+	//code := r.Form.Get("code")
+	//code := r.PostForm.Get("code")
+	fmt.Println(code)
 	//创建RJS引擎
 	var engine engine.RJSEngine
 	engine.Init()
 	resultString := ""
-	output := func(call otto.FunctionCall){
-		for n := 0;;n ++ {
-			Data := call.Argument(n)
-			if Data.IsNull(){
+	engine.Js.Set("output", func(call otto.FunctionCall) otto.Value {
+		//js.Call(call.Argument(0).String(),"")
+		n := 0
+		for {
+			value := call.Argument(n)
+			if !value.IsDefined() {
 				break
 			}
-			resultString += Data.String()
+			n++
+			resultString += value.String()
 		}
-		return
-	}
-	engine.Js.Set("output",output)
+		//return otto.Value{temp,"output"}
+		return otto.Value{}
+		//return otto.TrueValue()
+	})
 	engine.SetsandBoxMode(true,string(rand.Int()))
-	end := make(chan int)
+	//end := make(chan int)
+	ok := false
+	finished := make(chan int)
 	go func(){
 		select{
-			case <-end:
+			case <-time.After(time.Second * 5):
+				finished <- 1
 				return
 			default:
-				value,err := engine.Js.Run(code)
-				resultString += value.String()
+				_,err := engine.Js.Run(code)
+				ok = true
+				//resultString += value.String()
 				if err != nil{
 					resultString = err.Error()
 				}
+				if resultString == ""{
+					resultString = "Okay,No output."
+				}
+				w.Write([]byte(resultString))
+				finished <- 1
+				fmt.Println(resultString)
 				return
 		}
 	}()
-	time.After(time.Second * 5 )
+	<- finished
+	close(finished)
 	if resultString == ""{
 		resultString = "Timeout,No Output."
 	}
 	w.Write([]byte(resultString))
 	return
-}
-func base64Decode(src []byte) ([]byte, error) {
-	return coder.DecodeString(string(src))
 }
